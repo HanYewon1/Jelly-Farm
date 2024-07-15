6d8f0d25a8f9e3e51914a66da5f2ea28a3226ecf
Yewon Han <cherry01614@naver.com>
2024-07-16 02:45:14 +0900

    Initial commit

---
+++ Assets/Scripts/JellyController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float minX, maxX, minY, maxY;
+    private Vector2 JellyPosition;
+    private Vector2 nextPosition;
+    private Animator _animator;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        StartCoroutine(MovePause());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = nextPosition * speed * Time.deltaTime;
+    }
+
+    IEnumerator MovePause()
+    {
+        while (true) //2초동안 움직임 실행 후 1초 대기
+        {
+            RandomMove();
+            yield return new WaitForSeconds(3);
+            nextPosition = Vector2.zero;
+            _animator.SetBool("isMove", false);
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+    void RandomMove() //랜덤 방향으로 이동
+    {
+        nextPosition = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
+        JellyPosition = transform.position;
+        //왼쪽으로 이동 시 flipX 처리
+        if (nextPosition.x < 0) //왼쪽
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            _animator.SetBool("isMove", true);
+        }
+        else if(nextPosition.x > 0) //오른쪽
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            _animator.SetBool("isMove", true);
+        }
+        else //움직이지 않을 경우
+        {
+            _animator.SetBool("isMove", false);
+        }
+    }
+
+  
+    
+        
+    
+}
